982e97fd81c8ea048c893e838add6ae1c173097d
Subin Cho <sbjo7713@gmail.com>
2022-04-25 12:45:09 +0900

    커스텀 객체 @RequestParam 컨버터 테스트

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,11 @@ public class HelloController {
         return "ok";
     }
 
+    @GetMapping("/ip-port")
+    public String helloV3(@RequestParam IpPort ipPort) {
+        System.out.println("ip = " + ipPort.getIp());
+        System.out.println("port = " + ipPort.getPort());
+        return "ok";
+    }
 
 }
